a2cc8b1f7a40996136cf3764350c4ebba7f07e67
ArdillaV21 <fedevalle21@gmail.com>
2024-07-22 23:04:37 -0300

    Trabajando BackEnd con prisma y Mongo(autenticacion de usuarios)

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -6,6 +6,7 @@ import Modal from "../Modal";
 const LoginModal = () => {
 
     const loginModal = useLoginModal();
+    
     const [email, setEmail] = useState('');
     const[password, setPassword] = useState('');
     const [isLoading, setIsLoading] = useState(false);
